f194ee12263776faf2c53c733443062ea3d0361c
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-02-19 20:07:27 -0500

    Update Grid.hx

--- source/charting/ui/Grid.hx
+++ source/charting/ui/Grid.hx
@@ -1,21 +1,30 @@
 package charting.ui;
 
+import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
-import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 
 class Grid extends FlxSpriteGroup {
-	var grid(default, null):FlxBackdrop;
+	var grid(default, null):FlxSprite;
+	var gridWidth:Int = 55;
+
+	var curSection:Int = 0;
 
 	public function new(chart:fv.song.Chart.ChartJson):Void {
 		super();
 
-		grid = new FlxBackdrop('assets/images/charting/grid.png');
-		grid.blitMode = MAX_TILES_XY(16, 4);
+		grid = FlxGridOverlay.create(gridWidth, gridWidth, gridWidth * 8, gridWidth * (chart.Meta.TimeSignature.Steps *
+			chart.Meta.TimeSignature.Beats * chart.Meta.TimeSignature.Bars),
+		true, 0xFFFFFFFF, 0xFFC3C3C3); // Hmm... This might actually be better
+		grid.scrollFactor.set(1, 1);
+		grid.active = false;
+		grid.updateHitbox();
+		grid.screenCenter(X);
 		add(grid);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
-		grid.x -= 1.0;
+		grid.scroll.y;
 	}
 }
